cecc2073db8225f3b86e105904c19ab37243cd0c
Capi03 <cinthiacarrasco321@gmail.com>
2025-11-10 02:35:31 -0400

    cambios en otra rama

--- si2Backend/settings.py
+++ si2Backend/settings.py
@@ -40,7 +40,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://*.onrender.com',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    '*'
+    
 ]
 
 
@@ -142,11 +142,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Cloudinary Configuration
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': env('CLOUDINARY_API_KEY'),
-    'API_SECRET': env('CLOUDINARY_API_SECRET'),
-}
+#CLOUDINARY_STORAGE = {
+ #   'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+  #  'API_KEY': env('CLOUDINARY_API_KEY'),
+   # 'API_SECRET': env('CLOUDINARY_API_SECRET'),
+#}
 
 # Set Cloudinary as default file storage
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
